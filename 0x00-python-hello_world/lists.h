7996b403283c9e986ead883ea90480c230be193e
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-01 16:53:43 +0100

    updated header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -20,4 +20,9 @@ typedef struct listint_s
 	struct listint_s *next;
 }listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle9listint_t *list);
+
 #endif
