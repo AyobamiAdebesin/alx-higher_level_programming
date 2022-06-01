804492c560f02d8a4d3f5e48a1574505c3c5c995
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-01 19:35:50 +0100

    refactor: fixed bug in header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -23,6 +23,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int check_cycle9listint_t *list);
+int check_cycle(listint_t *list);
 
 #endif
