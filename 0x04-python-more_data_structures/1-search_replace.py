5771cd1bd0e671823e335080062813491f79639e
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-08 14:54:32 +0100

    refactor: fixed bug

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    new_list = my_list
+    for i in range(len(new__list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
