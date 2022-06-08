2b0c67b4153c88d9504246210e20e3e9ade1a034
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-08 16:33:29 +0100

    refactor: fixed bug

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     square_matrix = matrix.copy()
     for i in range(len(square_matrix)):
-        square_matrix[i] = list(map(lambda x: x**2, square_matrix[i]) )
+        square_matrix[i] = list(map(lambda x: x**2, square_matrix[i]))
     return square_matrix
