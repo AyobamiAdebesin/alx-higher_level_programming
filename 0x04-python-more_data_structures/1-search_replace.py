aa7397a52066ecff1d6ae39247d657727b7f6700
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-08 14:58:38 +0100

    refactor: fixed bug

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if not my_list:
+        return
     new_list = my_list
     for i in range(len(new_list)):
         if new_list[i] == search:
