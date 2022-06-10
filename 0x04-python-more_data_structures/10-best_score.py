2dc2b55e247e49c35884d72724582d83fd64e319
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-10 14:31:27 +0100

    Return Best Key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    dict_keys = list(a_dictionary.keys())
+    max_score = a_dictionary[dict_keys[0]]
+    for key in dict_keys:
+        if a_dictionary[key] > max_score:
+            max_score = a_dictionary[key]
+            return key
