a52cdcaee29a1b0fa50da4ebf8b2194d72770401
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-10 15:31:50 +0100

    Highest value in dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
     else:
         dict_keys = list(a_dictionary.keys())
-        max_key = ""
+        max_key = None
         max_score = a_dictionary[dict_keys[0]]
         for key in dict_keys:
             if a_dictionary[key] > max_score:
