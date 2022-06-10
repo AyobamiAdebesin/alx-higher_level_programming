05ab20105e28b98a176f604f2733cbac81b7098a
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-10 11:55:41 +0100

    search and replace an element in alist

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return
-    new_list = my_list
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
