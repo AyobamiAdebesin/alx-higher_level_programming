98087d309c0ba3e8d592b8a5a416085670e80fd8
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-10 15:19:16 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
+def best_score(a_dictonary):
     if not a_dictionary:
         return None
     dict_keys = list(a_dictionary.keys())
     max_score = a_dictionary[dict_keys[0]]
+    max_key = ""
     for key in dict_keys:
         if a_dictionary[key] > max_score:
             max_score = a_dictionary[key]
-            return key
+            max_key = key
+    return max_key
