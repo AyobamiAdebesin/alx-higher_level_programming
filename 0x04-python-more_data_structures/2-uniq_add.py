0630a6ab8c3cef85f150c6ee9749a131d710bfbd
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-10 11:57:44 +0100

    Unique addition in alist

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
+    sum_list = 0
     uniq_list = []
     for i in range(len(my_list)):
         if my_list[i] not in uniq_list:
             uniq_list.append(my_list[i])
-            sum += my_list[i]
-    return sum
+            sum_list += my_list[i]
+    return sum_list
