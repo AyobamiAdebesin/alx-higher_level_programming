c7a7dfe8d1d56280498da6b349156c555833547f
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 13:03:30 +0100

    refactor: fixed code to meet requirement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-            element_count += 1
         except:
             break
+        else:
+            element_count += 1
     print()
     return (element_count)
