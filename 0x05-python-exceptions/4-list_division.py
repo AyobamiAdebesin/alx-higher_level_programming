06319f9f88af45f7d0040390e827e10d6f915672
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 18:58:34 +0100

    refactor: fixed pycodestyle error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print('out of range')
         finally:
             new_list.append(result)
-    return (new_list)
-    
+    return (new_list)   
