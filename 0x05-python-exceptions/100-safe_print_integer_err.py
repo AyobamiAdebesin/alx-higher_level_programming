906bb8f34c87ce929051f1261348821a1c5071f5
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 22:33:37 +0100

    Print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        sys.stderr.write("Exception:", err)
+        return False
+    else:
+        return True
