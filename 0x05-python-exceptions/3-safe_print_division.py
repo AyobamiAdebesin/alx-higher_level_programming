313a2d7215ce853fbadaca628fd1283820e41957
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 18:05:02 +0100

    Catching ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    finally:
-        print("Inside Result: {}".format(result))
+    finally:i
+        print("Inside result: {}".format(result))
     return result
