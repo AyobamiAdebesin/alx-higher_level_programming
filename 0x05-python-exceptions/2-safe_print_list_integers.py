f767a184638b6f2f02a12b583e7b76a3503a21cb
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 15:41:52 +0100

    refactor: fixed bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i], end=''))
         except (ValueError, TypeError):
-            continue
-        element_count += 1
+            pass
+        else:
+            element_count += 1
     print()
     return element_count
-    
