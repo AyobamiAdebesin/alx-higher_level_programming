20a66353b7736155ddbe56207aeb4c3d53795b32
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 22:47:27 +0100

    Execute a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,4 +4,5 @@ def safe_function(fct, *args):
         result = fct(*args)
     except Exception as err:
         sys.stderr.write('Exception: '+ str(err) +'\n')
+        return None
     return result
