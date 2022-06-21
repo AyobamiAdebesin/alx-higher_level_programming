b95a353309ec5a4d709756ca306a4f4aaae2b27b
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 22:53:33 +0100

    refactor: fixed import error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
     except Exception as err:
         sys.stderr.write('Exception: '+ str(err) +'\n')
-        return None
+        result = None
     return result
