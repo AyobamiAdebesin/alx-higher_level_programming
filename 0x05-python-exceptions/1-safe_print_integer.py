20d0c6e761afd55d4a4ccddceb5168d5aaf7b7ac
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-21 13:20:54 +0100

    refactor: fixed pycodestyle error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        print("{}".format(value))
+    except ValueError:
         return False
     else:
         return True
