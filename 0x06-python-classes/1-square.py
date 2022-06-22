67af8a784e31ed9a244d8e80459fe958a74704e9
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-22 12:05:01 +0100

    A class that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" An empty class that defines a square"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+
+    """
+    def __init__(self):
+        """
+        Initializes the instance attributes of the class
+
+        Args:
+            self (None): The initialization parameter
+        Return:
+            None
+
+        """
+        self.__size = 20
+    pass
