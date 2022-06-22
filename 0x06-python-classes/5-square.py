4cb88fa276465cec6c720e947ec9ab71ceb3abe0
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-22 17:29:21 +0100

    Print the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -54,5 +54,4 @@ class Square:
         if self.__size == 0:
             print("{}".format(0))
         for i in range(self.__size):
-            print("#"*self.__size)
-            print("\n")
+            print("#"*self.__size, end='\n')
