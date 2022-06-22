515763f19cc4a3983522faa20668ca0b44041e54
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-22 12:51:26 +0100

    Added a function that calculates the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" An empty class that defines a square"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the instance attributes of the class
+
+        Args:
+            self (None): The initialization parameter
+        Return:
+            None
+
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        A method that calculates the area of the square
+        
+        """
+        return (self.__size)**2
