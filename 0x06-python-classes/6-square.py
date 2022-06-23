2c5d13e4cb73442e18c0ddb02bcea77f05687f20
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-23 08:31:14 +0100

    refactor: fixed bug in code; TypeError

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,7 +75,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position):
+            for i in range(self.__position[1]):
                 print()
             for i in range(0, self.__size):
                 for k in range(self.__position[0]):
