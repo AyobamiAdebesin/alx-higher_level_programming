f656ca1270ed43bea9b63da4ce67ec385836a7b6
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-23 23:15:59 +0100

    Printing a square by adding a -_str__ method

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -14,19 +14,6 @@ class Square:
         self.size = size
         self.position = position
 
-    def __str__(self):
-        """
-        Define a print method for a square
-       """
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end='') for j in range(0, self.__position[0])]
-            [print("#", end='') for k in range(0, self.__size)]
-            if i !+ self.__size - 1:
-                print("")
-        return ("")
-
     @property
     def size(self):
         """Get/set the current size of the square."""
@@ -69,3 +56,14 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
