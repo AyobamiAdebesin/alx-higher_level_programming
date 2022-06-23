5a571021ed5c0b56427d4c1647ac9d364b74abe3
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-23 16:17:00 +0100

    refactor: fixed pycodestyle error

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -73,4 +73,4 @@ class SinglyLinkedList:
         while tmp is not None:
             values.append(str(tmp.data))
             tmp = tmp.next_node
-        return ('\n'.join(values)) 
+        return ('\n'.join(values))
