1a115f8bc09b13b299cf7640eefef979e3d7806c
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-28 03:19:23 +0100

    Fixed error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """A class that defines a Rectangle"""
     def __init__(self, width=0, height=0):
         """ A method that initializes the instance attributes"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
