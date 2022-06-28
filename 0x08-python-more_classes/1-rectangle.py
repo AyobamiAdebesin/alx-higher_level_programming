521f03e64b7e22b31027753d94a760c0a07e1501
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-28 03:13:30 +0100

    Definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,14 +7,14 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ A method that initializes the instance attributes"""
 
-        if not isinstance(value, int):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = width
 
-        if not isinstance(value, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
