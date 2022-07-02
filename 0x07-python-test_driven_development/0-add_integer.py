9807241b00b0e92632386fdfe87e9a3431059ff2
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-02 15:29:57 +0100

    refactor: fixed pycodestyle error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,17 @@
-"""
+#!/usr/bin/python3
+""" A module that add two integers together"""
+
+
+def add_integer(a, b=98):
+    """
+    A function that adds two integers together
+
+    """
+    result = 0
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstanc(b, int) and not isinstance(b, float)):
+        raise TypeError("b mus be an integer")
+    else:
+        result = int(a) + int(b)
+    return result
