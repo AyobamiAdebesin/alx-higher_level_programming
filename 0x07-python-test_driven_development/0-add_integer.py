5ee11216f5a84bdc1df0544e7f9b03395cd6fa6a
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-02 16:12:20 +0100

    refactor: fixed syntax error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,7 +16,7 @@ def add_integer(a, b=98):
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int) and not isinstance(b, float)):
-        raise TypeError("b mus be an integer")
+        raise TypeError("b must be an integer")
     else:
         result = int(a) + int(b)
 
