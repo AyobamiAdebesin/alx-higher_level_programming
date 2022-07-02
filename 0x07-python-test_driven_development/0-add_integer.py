58c7be8ed53361eca4010b0f55e1c8568aa79ed4
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-02 15:41:25 +0100

    made script executable

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     result = 0
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if (not isinstanc(b, int) and not isinstance(b, float)):
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b mus be an integer")
     else:
         result = int(a) + int(b)
