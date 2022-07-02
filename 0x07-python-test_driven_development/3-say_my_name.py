5ea4a64613515fb281156843f143d02fa3f8e10b
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-02 19:41:26 +0100

    refactor: fixed pycodestyle error

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,28 +3,25 @@
 This module contains a function that prints a name
 """
 
+
 def say_my_name(first_name, last_name=""):
-        """
-        
-        A function that prints a name in full
 
-        Args:
-                first_name: The first name (string)
-                last_name: The last name(string)
-        
-        Return:
-                None
+    """
+    A function that prints a name in full
 
-        Raises:
-                TypeError: if first name is not a string
-                TypeError: if last_name is not a string
+    Args:
+        first_name: The first name (string)
+        last_name: The last name(string)
+    Return:
+        None
+    Raises:
+        TypeError: if first name is not a string
+        TypeError: if last_name is not a string
+    """
 
-        """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
 
-        if type(first_name) is not str:
-                raise TypeError("first_name must be a string")
-
-        if type(last_name) is not str:
-                raise TypeError("last_name must be a string")
-        
-        print("My name is {} {}".format(first_name, last_name))
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
