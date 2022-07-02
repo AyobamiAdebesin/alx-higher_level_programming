30c799582fbd54d926c2b1f54dc1e3ad0ef5dfb1
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-02 15:57:51 +0100

    refactor: added more documentatiton

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,12 +6,18 @@ def add_integer(a, b=98):
     """
     A function that adds two integers together
 
+    Float arguments are typecasted to int before addition
+
+    Raises:
+        TypeError: If either a or b is non-integer and non-float
     """
     result = 0
+
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b mus be an integer")
     else:
         result = int(a) + int(b)
+
     return result
