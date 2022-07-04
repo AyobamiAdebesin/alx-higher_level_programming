3268b20e55257db10d839fd5ba944c87b7971a35
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-04 16:38:03 +0100

    refactor: fixed string formatting

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
