376adc043fe85466292b76cb4d14eebebdb3af56
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-04 14:28:47 +0100

    changed permissions for script

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the empty BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """ A class the defines a geometry object"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
