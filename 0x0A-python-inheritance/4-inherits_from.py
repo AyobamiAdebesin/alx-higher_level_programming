5345d996814ccefdbdc1797e901835e3d235120b
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-04 13:38:31 +0100

    refactor: new logic

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,7 @@ def inherits_from(obj, a_class):
     """
     A  function that checks if an object is an instance of a class
     """
-    return issubclass(obj, a_class)
+    if type(obj) is a_class:
+        return False
+    else:
+        return isinstance(obj, a_class)
