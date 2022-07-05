e437cac5bfcb1d7dcda44ba60cde8045ce5fae7e
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-05 14:48:43 +0100

    Rebel Integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains a class that inherits from the int class"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from the int class, but guess what?
+    
+    ............Its a rebel...........Ahahha!!!
+    """
+
+    def __eq__(int1, int2):
+        if int1 != int2:
+            return False
+
+    def __neq__(int1, int2):
+        if int1 == int2:
+            return True
