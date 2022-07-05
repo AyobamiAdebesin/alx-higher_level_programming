24cf50331f44ebf6ec7ec7a00906cff83992d904
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-05 15:23:13 +0100

    Setting attributes for an object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds
+a new atribute to an object if possible
+"""
+
+def add_attribute(obj, name, value):
+    """ Function that adds a new attribute to an object
+
+    Args:
+        obj: object
+        name: attribute name
+        value: attribute value
+
+    Raises:
+        TypeError: when the attribute can't be added
+
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("cant add new attribute")
+setattr(obj, name, value)
