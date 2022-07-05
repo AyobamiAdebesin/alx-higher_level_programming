20158fc42d4dc73a946841c3f8fb41b83d86c42e
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-05 13:46:20 +0100

    Full Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module contains a class that inherits from the BaseGeometry class
+
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ A class that inherits from the BaseGeometry class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self, width, height):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[{} {}/{}]".format(type(self), self.__width, self.__height))
