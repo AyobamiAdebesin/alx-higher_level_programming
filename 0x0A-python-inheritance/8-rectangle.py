b7a6b194fb5e2fa236af6824fe3d4a876f6c3c13
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-05 13:32:13 +0100

    refactor: fixed bug in code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-This module contains a class that inherits from a super class
+This module contains a class that inherits from the BaseGeometry class
 
 """
-from 7-base-geometry import BaseGeometry
 
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """
-    A class that inherits from a super class
-
-    """
+    """ A class that inherits from the BaseGeometry class"""
 
     def __init__(self, width, height):
-        self.integer_validator(name="width", value=width)
-        self.integer_validator(name="height", value=height)
-        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
+        self.__height = height
