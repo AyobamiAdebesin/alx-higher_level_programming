76f0fe198cd2a3117bfe3cd6bd849a5d1cf56e6e
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-06 14:13:48 +0100

    JSON to Python object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a function that deserializes an object"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    A function that deserializes a JSON string to a Python Object
+    """
+    return (json.load(my_str))
