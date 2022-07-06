d3ce05f9ae3702baa0fb5101edbaf5749e0428d4
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-06 13:27:21 +0100

    changed mode to write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,9 +2,8 @@
 """This module contains a function that writes to a file"""
 
 
-def write_file(filename="", mode='w', text=""):
+def write_file(filename="", text=""):
     """A function that writes to a file"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         n_chars = f.write(text)
     return n_chars
-
