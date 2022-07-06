5ae412415d27d2f98ad229c9157ca209c3e8a506
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-06 14:08:31 +0100

    JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that serializes an object to json
+"""
+import json
+
+def to_json_string(my_obj):
+    """
+    A function that returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
