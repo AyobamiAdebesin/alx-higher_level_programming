5a8d8b466361b5ebd524c2ae62c4e8593f042afd
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-06 13:23:17 +0100

    writing to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """This module contains a function that writes to a file"""
 
 
-def write_file(filename="", text=""):
+def write_file(filename="", mode='w', text=""):
     """A function that writes to a file"""
     with open(filename, encoding="utf-8") as f:
         n_chars = f.write(text)
