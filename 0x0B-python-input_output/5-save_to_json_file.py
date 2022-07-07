06401fe55b676583795bbf770cea63b83e1f02a3
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-07 13:27:55 +0100

    refactor: fixed pycodestyle error

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains a function that writes an 
+This module contains a function that writes an
 object to a text file using a JSON representation
 """
 import json
@@ -8,6 +8,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """A function that save to JSON"""
-
-   with open(filename, mode='w', encoding='utf-8') as f:
-       json.dump(my_obj, f)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
