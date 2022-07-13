1432be81d8f0d377b20f6f36ed9a672626a1dbf9
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-13 05:13:49 +0100

    refactor: added dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -70,3 +70,12 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
