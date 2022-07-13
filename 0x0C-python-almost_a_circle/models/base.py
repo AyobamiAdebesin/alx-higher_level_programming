7a04fc8c05dcd5291db49f11b06bc7180463ae9d
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-13 03:32:17 +0100

    Added the base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module defines a base model class"""
+
+
+class Base:
+    """ A Base model class
+
+    Base: Rrpresents the "base" for all other classes
+
+    Attributes:
+        __nb_objects(int): The number of instances
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializes a new Base class.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+
+        if id not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
