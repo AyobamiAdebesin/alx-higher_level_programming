26ca039be79cfc29e7a8f0be38d7502494939083
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-13 04:50:35 +0100

    refactor: changed import mode for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module contains a Rectangle class that inherits from Base class"""
 
-from models.base import Base
+Base = __import__("base").Base
 
 
 class Rectangle(Base):
