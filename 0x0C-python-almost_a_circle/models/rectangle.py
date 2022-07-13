40af7dd2a9c9329597897dba541fb60088edfc58
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-13 04:26:49 +0100

    added display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,4 +87,7 @@ class Rectangle(Base):
         """Print the Rectangle using the `#` character."""
         if self.width == 0 or self.height == 0:
             print("")
-            return
+        else:
+            hash_width = "#"*self.width
+            for i in range(self.height):
+                print(hash_width)
