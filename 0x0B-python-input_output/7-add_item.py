b046a3cda5be307d06f8719f52d8d5234a9f1aad
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-02 04:27:54 +0100

    refactor: completed implementation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ 
 This module contains a script that adds all arguments
-to a Python lis and then save them to a file
+to a Python list and then save them to a file
 """
-import os
 import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_josn_file = __import__("6-load_from_json_file").load_from_json_file
-
-my_list = []
-filename = "add_item.json"
-
-if os.path.exists(
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
