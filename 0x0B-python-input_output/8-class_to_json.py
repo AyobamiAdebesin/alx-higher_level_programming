38969de1de20467c45fcc39d0ae6d4f8cc477d43
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-02 04:45:53 +0100

    added new script

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module serializes a class to a JSON object"""
+import json
+
+
+def class_to_json(obj):
+    """ Serializes obj to JSON """
+    my_dict = {}
+    for key, value in (obj.__dict__).items():
+        my_dict[key] = value
+    return my_dict
