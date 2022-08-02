6f0aa3c602806ee14c426996b1ce9caec5a80954
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-02 04:29:47 +0100

    refactor: completed implementation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 This module contains a script that adds all arguments
 to a Python list and then save them to a file
 """
@@ -7,7 +7,8 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    load_from_json_file = __import__(
+            "6-load_from_json_file").load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
