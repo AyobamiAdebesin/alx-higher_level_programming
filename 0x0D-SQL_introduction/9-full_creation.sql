c96854ace1a5f43edee54a783242979766a1f7c1
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-10 13:17:34 +0100

    full creaion of table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS second_table(
 INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table(id ,name, score) VALUES (3, "Bob", 14);
-INSERT INTO second_table(id, name, score0 VALUES (4, "George", 8);
+INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
