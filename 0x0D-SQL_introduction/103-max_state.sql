d23af34108b13d4d2834830f930cc8a98c62f748
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-10 15:29:55 +0100

    refactor query

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,6 @@
--- Display max temp of each state
+-- Script that displays the max temperature of each state
+-- Query to display the max temperature of each state
 SELECT state, MAX(value) AS max_temp
 FROM temperatures
-ORDER BY state
+GROUP BY state
 LIMIT 3;
