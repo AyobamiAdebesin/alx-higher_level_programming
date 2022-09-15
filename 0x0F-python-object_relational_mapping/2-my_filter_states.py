6aef71cf962058463cd6dd065933867f32e8f6a5
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-15 14:25:50 +0100

    Filter by name passed on the command line

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A script that takes in argument and displays
+all values in the states table
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3], host="localhost")
+    cur = db.cursor()
+
+    sql = f"SELECT * FROM states WHERE states.name='{sys.argv[4]}'"
+    cur.execute(sql)
+    for result in cur.fetchall():
+        print(result)
+    db.close()
