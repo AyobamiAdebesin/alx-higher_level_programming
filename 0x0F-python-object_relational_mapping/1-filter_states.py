8e7fa5ddeeb185dddf41ca754524a08b91b4a81c
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-15 14:12:51 +0100

    [Refactor]: changed method of filtering

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
             db=sys.argv[3], host="localhost")
     cur = db.cursor()
     sql = "SELECT * FROM states ORDER BY states.id ASC"
+    # Alternatively
+    # SELECT * FROM states WHERE states.name LIKE
+    # 'N%' ORDER BY states.id ASC
     try:
         cur.execute(sql)
         for result in cur.fetchall():
