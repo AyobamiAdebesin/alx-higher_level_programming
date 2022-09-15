afaaf866c8fbbbea23ccd45c71154b9bda3c2b08
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-15 14:10:35 +0100

    [Refactor]: changed method of filtering

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,16 +11,12 @@ if __name__ == "__main__":
             user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3], host="localhost")
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE states.name LIKE \
-        'N%' ORDER BY states.id ASC"
-    cur.execute(sql)
-    for result in cur.fetchall():
-        print(result)
-
-    # try:
-    #     cur.execute(sql)
-    #     for result in cur.fetchall():
-    #         print(result)
-    # except Exception:
-    #     print("Unable to fetch data from database")
+    sql = "SELECT * FROM states ORDER BY states.id ASC"
+    try:
+        cur.execute(sql)
+        for result in cur.fetchall():
+            if result[1][0] == "N":
+                print(result)
+    except Exception:
+        print("Unable to fetch data from database")
     db.close()
