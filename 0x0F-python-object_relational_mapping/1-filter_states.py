815bece0e5789fc043b4283fba651dd1f97b85d6
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-15 13:59:12 +0100

    [Refactor]: fixed pycodestyle error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
             user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3], host="localhost")
-    
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC"
+    sql = "SELECT * FROM states WHERE states.name LIKE \
+        'N%' ORDER BY states.id ASC"
     try:
         cur.execute(sql)
         results = cur.fetchall()
@@ -20,5 +20,4 @@ if __name__ == "__main__":
             print(result)
     except Exception:
         print("Unable to fetch data from database")
-    
     db.close()
