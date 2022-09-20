9b57634dadd84bcb0e20079a6b68bbf36ccf9c80
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 18:38:33 +0100

    update an object from the database

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A script that updates name of a State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    stateUpdated = session.query(State).filter_by(State.id == 2)
+    if stateUpdated:
+        stateUpdated.name = "New Mexico"
+        session.commit()
