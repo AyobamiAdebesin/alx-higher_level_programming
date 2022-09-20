697b06b88f19632fcae48f119e08ec65457f7642
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 16:58:39 +0100

    Print out first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,12 +10,11 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@\
-            localhost:3306/{sys.argv[3]}"
-    )
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
+            @localhost:3306/{sys.argv[3]}")
     Session = sessionmaker(bind=engine)
     session = Session()
 
     first_state = session.query(State).order_by(State.id).first()
-    print("Nothing" if not state else "{}: {}".\
+    print("Nothing" if not first_state else "{}: {}".\
         format(first_state.id, first_state.name))
