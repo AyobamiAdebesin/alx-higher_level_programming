2ca9aecb7d54189b21fce609976eeff525ed8085
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 16:51:47 +0100

    Print out first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
 
     first_state = session.query(State).order_by(State.id).first()
     if first_state:
-        print(first_state)
+        print(f"{first_state.id}: {first_state.name}")
     else:
         print("Nothing")
