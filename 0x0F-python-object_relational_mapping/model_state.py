a1aaf11b614b8cc5668a5bf62f14dd347b17c7a3
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 14:17:39 +0100

    Added model for state tables

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that contains the class definition of a 
+State and an instance of the declarative_base class
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Column, Table, Integer, DateTime, Text, MetaData
+
+my_metadata = MetaData()
+Base = declarative_base(metadata=my_metadata)
+
+class State(Base):
+    """
+    A class that represents a state with the id and name
+    atribute
+    """
+    __tablename__ = 'states'
+    id = Column(Integer(), unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
