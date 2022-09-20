75232de45ad52c86d9f9a8957ffd37ab5b4e1a5c
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 11:54:48 +0100

    filter cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
         print(", ".join([state[1] for state in states]))
     except Exception:
         print("Unable to retrieve data from table")
-    
+
     db.close()
