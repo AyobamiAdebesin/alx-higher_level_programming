0bce1ddcf02096c22ab2e5437bfd06a5d3710aab
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 17:34:44 +0100

    [Refactor]: fix error for not passing checks

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,13 +10,12 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@\
-            localhost:3306/{sys.argv[3]}")
-
-    # states = session.query(State).filter_by(State.name.like(%a%)).all()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+    # states = session.query(State).filter_by(State.name.like(%a%)).all()
 
     for state in session.query(State).order_by(State.id):
         if "a" in state.name:
