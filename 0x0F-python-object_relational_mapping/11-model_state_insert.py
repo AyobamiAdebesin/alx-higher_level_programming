bd9a82a876ff6164e61e637671d6365c12b9f969
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 17:59:30 +0100

    Add a new object to the table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A script that adds the State object "Louisiana"
+to the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State("Louisiana")
+    session.add(new_state)
+    session.commit()
+    for state in session.query(State).filter_by(State.name == "Louisiana"):
+        print(state.id)
