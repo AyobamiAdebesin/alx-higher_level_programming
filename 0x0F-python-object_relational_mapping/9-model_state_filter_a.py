02664312d1acf7ad1413ff6a73b66a51e110a665
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 17:18:02 +0100

    filter state by letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter_by(State.name.like('%a%')).all()
+    states = session.query(State).filter_by(
+        State.name.like('%a%')).order_by(State.id).all()
     for state in states:
         print(f"{state.id}: {state.name}")
