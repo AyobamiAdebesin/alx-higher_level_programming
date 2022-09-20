ba4b942be33aeb8f3d2d6c17cdaf03f8d04ee45b
AyobamiAdebesin <froshofficial@gmail.com>
2022-09-20 23:19:22 +0100

    List all cities

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_city = session.query(State, City).filter(State.id == City.state_id).all()
+    state_city = session.query(State, City).filter(
+        State.id == City.state_id).all()
     for state, city in state_city:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
