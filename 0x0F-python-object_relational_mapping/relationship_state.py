43bd73cfea4f28dad78f4f75e40827ba33eb87dc
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-08 13:03:09 +0100

    Refactor: Changed backref from states -> state

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -21,4 +21,4 @@ class State(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="states", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all, delete")
