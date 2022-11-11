80494af798750345c6053965db351dc1894743f6
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-11 13:00:58 +0100

    Display github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A script that takes your GitHub credentials and uses
+    the GitHub API to display your id
+"""
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
