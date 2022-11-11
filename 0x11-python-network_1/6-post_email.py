757428cc7e0ee265e4870acdac94894d411c4427
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-11 11:58:54 +0100

    Checked task successfully

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ A script that takes in a URL and sends a POST request to
-the URL with an email as a parameter and displays the body of the response
+    the URL with an email as a parameter and displays the body
+    of the response
 """
 import requests
 import sys
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2])
     r = requests.post(url, data=value)
