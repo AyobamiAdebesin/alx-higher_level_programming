463462c15b75b44e73d0924857cbb85bc10b04dd
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-11 11:14:19 +0100

    Displays body of response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A script tha displays the body of a response after
+    sending a POST request with email
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    mail = sys.argv[2]
+    values = {"email": mail}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urlllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
+        rint(html)
