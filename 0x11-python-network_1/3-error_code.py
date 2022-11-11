19c62cd4dec96d2c459ac2289f1b969d7dc13c0d
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-11 11:22:57 +0100

    Another task

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Displays body of response after sending POST request with email
+while managing exceptions"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            html = response.read().decode('utf-8')
+            print(html)
+        except urllib.error.HTTPError as e:
+            code = e.code
+            print("Error code: {:d}".format(code))
