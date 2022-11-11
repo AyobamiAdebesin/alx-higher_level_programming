0d98e812a4a0d4bc808eb81d1b41d37fcff38940
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-11 11:17:53 +0100

    Fixed typo

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,10 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     mail = sys.argv[2]
-    values = {"email": mail}
+    values = {'email': mail}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         html = response.read().decode('utf-8')
-        rint(html)
+        print(html)
