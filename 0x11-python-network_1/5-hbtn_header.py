1213339dfbdcc1d2f808567bb269fb5b9a6a2753
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-11 11:51:28 +0100

    Task checked successfully

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A Python script that sends a request to a URL and extract 
+    information in the header of the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+    print(res.headers.get("X-Request-Id"))
