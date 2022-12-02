087a58e7c666a397faf7f3e051f6d290431192bc
AyobamiAdebesin <froshofficial@gmail.com>
2022-12-02 21:54:03 +0100

    Renamed file

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" A Python Script that fetches URL with the request library"""
-import requests
-
-if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status")
-    t = req.text
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(t), t))
